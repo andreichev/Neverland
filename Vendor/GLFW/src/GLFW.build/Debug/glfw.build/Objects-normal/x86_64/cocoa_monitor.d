45abc34f8bab8e2c3ae1bc0fb263d8c7a3a36e57
Andreichev Michail <andreichev.m@mail.ru>
2021-06-30 00:11:13 +0300

    first commit

---
+++ Vendor/GLFW/src/GLFW.build/Debug/glfw.build/Objects-normal/x86_64/cocoa_monitor.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/cocoa_monitor.m \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/internal.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/glfw_config.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/../include/GLFW/glfw3.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/cocoa_platform.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/posix_thread.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/cocoa_joystick.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/nsgl_context.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/egl_context.h \
+  /Users/admin/Projects/Neverland/Vendor/GLFW/src/osmesa_context.h
