af675cf783119c2a393fff0fdb25e8a258968de0
Andreichev Michail <andreichev.m@mail.ru>
2021-07-13 17:51:10 +0300

    texture implementation

--- resources/Vertex.glsl
+++ resources/Vertex.glsl
@@ -1,6 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) int vec2 aTexCoord;
+layout (location = 1) in vec2 aTexCoord;
+
+out vec2 TexCoord;
 
 void main()
 {
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,21 +1,29 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
-#include <iostream>
 #include "Shader.h"
 
 void processInput(GLFWwindow* window);
-void createVertexArray(unsigned int bufferId, unsigned int* vertexArrayId);
-void saveTriangleCoordsToVRAM(unsigned int* bufferId);
+void createVertexArray(unsigned int vertexBufferId, unsigned int indexBufferId, unsigned int* vertexArrayId);
+void saveDataToVRAM(unsigned int* vertexBufferId, unsigned int* indexBufferId);
+void saveTextureToVRAM(const char* path, unsigned int* textureId);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+    // positions          // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+     0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+};
+unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
 };
 
 int main()
@@ -43,14 +51,18 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    unsigned int grassTextureId;
+    saveTextureToVRAM("grass1.png", &grassTextureId);
     
     Shader base = Shader("Vertex.glsl", "Fragment.glsl");
 
-    unsigned int bufferId;
-    saveTriangleCoordsToVRAM(&bufferId);
+    unsigned int vertexBufferId;
+    unsigned int indexBufferId;
+    saveDataToVRAM(&vertexBufferId, &indexBufferId);
 
     unsigned int vertexArrayId;
-    createVertexArray(bufferId, &vertexArrayId);
+    createVertexArray(vertexBufferId, indexBufferId, &vertexArrayId);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -59,9 +71,11 @@ int main()
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, grassTextureId);
+        
         base.use();
         glBindVertexArray(vertexArrayId);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glUseProgram(0);
         glBindVertexArray(0);
 
@@ -70,28 +84,67 @@ int main()
     }
 
     glDeleteVertexArrays(1, &vertexArrayId);
-    glDeleteBuffers(1, &bufferId);
+    glDeleteBuffers(1, &vertexBufferId);
+    glDeleteBuffers(1, &indexBufferId);
     base.deleteProgram();
 
     glfwTerminate();
     return 0;
 }
 
-void saveTriangleCoordsToVRAM(unsigned int* bufferId) {
-    glGenBuffers(1, bufferId);
-    glBindBuffer(GL_ARRAY_BUFFER, *bufferId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+void saveTextureToVRAM(const char* path, unsigned int* textureId) {
+    glGenTextures(1, textureId);
+    glBindTexture(GL_TEXTURE_2D, *textureId); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    // set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 }
 
-void createVertexArray(unsigned int bufferId, unsigned int* vertexArrayId) {
-    glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+void saveDataToVRAM(unsigned int* vertexBufferId, unsigned int* indexBufferId) {
+    glGenBuffers(1, vertexBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, *vertexBufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    glGenBuffers(1, indexBufferId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *indexBufferId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void createVertexArray(unsigned int vertexBufferId, unsigned int indexBufferId, unsigned int* vertexArrayId) {
     glGenVertexArrays(1, vertexArrayId);
     glBindVertexArray(*vertexArrayId);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*) 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*) 0);
     glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void processInput(GLFWwindow* window)
