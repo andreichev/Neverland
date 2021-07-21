a06ed913e8fa79afe754aae739a7bf1c98703fc0
Andreichev Michail <andreichev.m@mail.ru>
2021-07-21 10:42:51 +0300

    camera class, cube

--- Neverland/src/Application.cpp
+++ Neverland/src/Application.cpp
@@ -51,29 +51,62 @@ int main()
     
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(NULL);
-
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    const float SIZE = 200.f;
     Vertex vertices[] = {
-        Vertex( glm::vec3(100.f, 100.f, -10.0f),  glm::vec2( 1.0f, 1.0f) ),
-        Vertex( glm::vec3(100.f, -100.f, -10.0f),  glm::vec2( 1.0f, 0.0f) ),
-        Vertex( glm::vec3(-100.f, -100.f, -10.0f),  glm::vec2( 0.0f, 0.0f) ),
-        Vertex( glm::vec3(-100.f, 100.f, -10.0f),  glm::vec2( 0.0f, 1.0f) )
+        // Front
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 0
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ), // 1
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 2
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ), // 3
+        // Back
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 4
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ), // 5
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 6
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ), // 7
+        // Top
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 8
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ), // 11
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 10
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ), // 9
+        // Bottom
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 12
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ), // 13
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 14
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ), // 15
+        // Left
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 16
+        Vertex( glm::vec3(-SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ), // 17
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 18
+        Vertex( glm::vec3(-SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ), // 19
+        // Right
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 0.0f, 1.0f ) ), // 20
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f, -SIZE / 2.0f), glm::vec2( 1.0f, 1.0f ) ),  // 23
+        Vertex( glm::vec3( SIZE / 2.0f,  SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 1.0f, 0.0f ) ), // 22
+        Vertex( glm::vec3( SIZE / 2.0f, -SIZE / 2.0f,  SIZE / 2.0f), glm::vec2( 0.0f, 0.0f ) ) // 21
     };
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
+    
+    unsigned int indices[36] = {
+         0,  1,  2,  2,  3,  0, // Front
+         4,  5,  6,  6,  7,  4, // Back
+         8,  9, 10, 10, 11,  8, // Top
+        12, 13, 14, 14, 15, 12, // Bottom
+        16, 17, 18, 18, 19, 16, // Left
+        20, 21, 22, 22, 23, 20  // Right
     };
+
     {
         Shader base = Shader("Vertex.glsl", "Fragment.glsl");
-        Texture grass1("grass1.png");
+        Texture grass1("arbuz.png");
         std::vector<Texture*> textures;
         textures.push_back(&grass1);
-        Mesh mesh = Mesh(vertices, 4, indices, 6, textures);
+        Mesh mesh = Mesh(vertices, 24, indices, 36, textures);
         
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(400.f, 400.f, 0.f));
-        // glm::mat4 projectionMatrix = glm::perspective(90.0f, 1.0f, 0.1f, 1000.0f);
-        glm::mat4 projectionMatrix = glm::ortho(0.0f, 800.0f, 0.0f, 800.0f, 0.f, 1.0f);
-        glm::mat4 model = glm::mat4(1.f);
+        glm::mat4 view = glm::rotate(glm::mat4(1.0f), glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 projectionMatrix = glm::perspective(90.0f, 1.0f, 0.1f, 1000.0f);
+        glm::mat4 model(1.f);
         
         base.use();
         base.setMat4("projection", projectionMatrix);
@@ -83,7 +116,7 @@ int main()
         // projection - transform to NDC
     
         glm::vec4 clearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glm::vec3 translate(0.f);
+        glm::vec3 translate(0.f, 0.f, 200.f);
         
         float time;
         while (!glfwWindowShouldClose(window))
@@ -92,13 +125,14 @@ int main()
             processInput(window);
 
             glClearColor(clearColor.x, clearColor.y, clearColor.z, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             base.use();
             base.setInt("texture1", 0);
-            model = glm::scale(glm::mat4(1.f), glm::vec3(abs(sin(time)) + 1.f, abs(sin(time)) + 1.f, 0.f));
-            model = glm::rotate(model, time, glm::vec3(0.f, 0.f, 1.f));
+            model = glm::mat4(1.f);
+            // model = glm::scale(glm::mat4(1.f), glm::vec3(abs(sin(time)) + 1.f, abs(sin(time)) + 1.f, 1.f));
             model = glm::translate(model, translate);
+            model = glm::rotate(model, time, glm::vec3(1.f, 1.f, 0.f));
             base.setMat4("view", view);
             base.setMat4("model", model);
             
@@ -110,8 +144,8 @@ int main()
             
             ImGui::Begin("Neverland begins...");
             ImGui::Text("Translation of land:");
-            ImGui::SliderFloat2("Translate", &translate.x, -300.f, 300.f);
-            ImGui::ColorEdit3("Clear color:", (float*)&clearColor); // Edit 3 floats representing a color
+            ImGui::SliderFloat3("Translate", &translate.x, -300.f, 300.f);
+            ImGui::ColorEdit3("Clear color:", (float*)&clearColor);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,ImGui::GetIO().Framerate);
             ImGui::End();
 
